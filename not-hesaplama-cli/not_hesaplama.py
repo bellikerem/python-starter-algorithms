5359d5e4c1b89c2af79c12c81184f8a065cd1e3c
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-11 21:26:42 +0300

    FEAT: Not Hesaplama: Harf notu (AA, DC, FF) karar mantığı ve nihai çıktı formatı uygulandı

--- not-hesaplama-cli/not_hesaplama.py
+++ not-hesaplama-cli/not_hesaplama.py
@@ -7,4 +7,19 @@ while i<=not_sayisi:
     i += 1
 notlar_toplamı = sum(girilen_notlar)
 ortalama = notlar_toplamı/not_sayisi
-print(ortalama)
+if ortalama>=90:
+    harf_notu = "AA"
+elif ortalama<90 and ortalama>=80:
+    harf_notu = "BA"
+elif ortalama<80 and ortalama>=70:
+    harf_notu = "BB"
+elif ortalama<70 and ortalama>=60:
+    harf_notu = "CC"
+elif ortalama<60 and ortalama>=50:
+    harf_notu = "DC"
+else:
+    harf_notu = "FF"
+
+print(f"Girdiğiniz notlar: {girilen_notlar}")
+print(f"Ortalamanız: {int(ortalama)}")
+print(f"Harf notunuz: {harf_notu}")
