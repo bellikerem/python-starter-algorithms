21ac953f4a72c4983a438d556f51c04892394950
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-13 17:57:37 +0300

    FEAT: Kelime Sayacı: Sözlük (Dictionary) yapısı kullanılarak kelime frekanslarını sayan ana algoritma uygulandı

--- kelime-sayacı/kelime-sayacı.py
+++ kelime-sayacı/kelime-sayacı.py
@@ -1,5 +1,9 @@
 import string
 
+sozluk = {
+
+}
+
 print("\n--- Kelime Sayacı ---")
 file = open("kelime-sayacı/kelime_sayacı.txt" , "w" , encoding = "utf-8")
 while True:
@@ -13,11 +17,23 @@ while True:
     except ValueError:
         print("Lütfen tam sayı girişi yapınız")
 file.close()
+
+
+
 with open("kelime-sayacı/kelime_sayacı.txt" , "r" , encoding = "utf-8") as file1:
     kelime_listesi = []
     for satır in file1:
-        temiz_satır = satır.strip(string.punctuation)
-        kelime_listesi = temiz_satır.lower().split()
-print(kelime_listesi)
+        satır_kelimeleri = satır.lower().split()
+        for kelime in satır_kelimeleri:
+            temiz_kelime = kelime.strip(string.punctuation)
+            if temiz_kelime:
+                kelime_listesi.append(temiz_kelime)
 
+    for i in kelime_listesi:
+        if i in sozluk:
+            sozluk[i] += 1
+        else:
+            sozluk[i] = 1
 
+print("\n--- SONUÇ ---")
+print(sozluk)
