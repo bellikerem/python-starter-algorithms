78694973764a801f3a3240979dfe819503912b06
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-15 19:58:34 +0300

    Temel Finans Hesaplayıcı: REFACTOR: Basit Faiz Hesaplayıcı modüler hale getirildi. FEAT: Tekrarlayan girdi alımını ve doğrulamasını yöneten yardımcı (helper) fonksiyon eklendi.

--- temel-finans-hesaplayıcı/temel-finans-hesaplayıcı.py
+++ temel-finans-hesaplayıcı/temel-finans-hesaplayıcı.py
@@ -8,5 +8,39 @@ while True:
         kullanici_secimi = int(kullanici_secimi_input)
         if kullanici_secimi not in [1,2,3,4]:
             print("Lütfen seçeneklerdeki sayılardan birini giriniz.")
+            continue
+        break
     except ValueError:
         print("Lütfen tam sayı girişi yapınız")
+
+
+def sayi_kontrolü(prompt,tip):
+    while True:
+        try:
+            deger = tip(input(prompt))
+            if deger<=0:
+                print("Lütfen 0'dan büyük bir sayı giriniz")
+                continue
+            return
+        except ValueError:
+            print("Lütfen tam sayı girişi yapınız")
+
+
+def basit_faiz_hesaplayici():
+    ana_para = sayi_kontrolü("Lütfen para miktarını (TL) giriniz: ", int)
+    faiz_orani = sayi_kontrolü("Lütfen yıllık faiz oranını (%) giriniz: ", float)
+    süre = sayi_kontrolü("Lütfen süreyi (ay) giriniz: ", int)
+    faiz_farki = ana_para * (faiz_orani/100) * süre
+    nihai_tutar = faiz_farki + ana_para
+    print("\n--- SONUÇ ---")
+    print(f"Faiz farkı: {faiz_farki}")
+    print(f"Nihai tutar: {nihai_tutar}")
+
+if kullanici_secimi == 1:
+    basit_faiz_hesaplayici()
+if kullanici_secimi == 2:
+    None
+if kullanici_secimi == 3:
+    None
+else:
+    quit
