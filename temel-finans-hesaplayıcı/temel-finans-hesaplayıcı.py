55cfdbdb434e35894a1ffb8f894f7c91688b2c52
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-15 20:46:47 +0300

    FEAT: Temel Finans Hesaplayıcı: KDV Hesaplayıcı (Ekleme/Çıkarma) fonksiyonu eklendi. Projenin tüm ana işlevleri tamamlandı.

--- temel-finans-hesaplayıcı/temel-finans-hesaplayıcı.py
+++ temel-finans-hesaplayıcı/temel-finans-hesaplayıcı.py
@@ -26,6 +26,7 @@ def sayi_kontrolü(prompt,tip):
             print("Lütfen tam sayı girişi yapınız")
 
 def basit_faiz_hesaplayici():
+    print("\n--- BASİT FAİZ HESAPLAYICI ---")
     ana_para = sayi_kontrolü("Lütfen para miktarını (TL) giriniz: ", int)
     faiz_orani = sayi_kontrolü("Lütfen yıllık faiz oranını (%) giriniz: ", float)
     süre = sayi_kontrolü("Lütfen süreyi (ay) giriniz: ", int)
@@ -36,6 +37,7 @@ def basit_faiz_hesaplayici():
     print(f"Nihai tutar: {nihai_tutar} TL")
 
 def taksit_hesapla():
+    print("\n--- TAKSİT HESAPLAYICI ---")
     kredi_miktari = sayi_kontrolü("Lütfen kredi miktarını giriniz: ", int)
     faiz_orani = sayi_kontrolü("Lütfen yıllık faiz oranını (%) giriniz: ", float)
     süre = sayi_kontrolü("Lütfen süreyi (ay) giriniz: ", int)
@@ -47,11 +49,38 @@ def taksit_hesapla():
     print("\n--- SONUÇ ---")
     print(f"Aylık kredi taksit ücretiniz {aylik_taksit} TL")
 
+def kdv_hesapla():
+    print("\n--- KDV HESAPLAYICI ---")
+    toplam_tutar = sayi_kontrolü("Lütfen toplam tutarı giriniz: ", int)
+    kdv = sayi_kontrolü("Lütfen KDV oranını (%) giriniz: ", int)
+    while True:
+        try:
+            karar_input = input("KDV Eklemek için 1\n"
+            "KDV Çıkarmak için 2'yi tuşlayınız: ")
+            karar = int(karar_input)
+            if karar not in [1,2]:
+                print("Lütfen 1 yada 2 yi tuşlayınız")
+                continue
+            break
+        except ValueError:
+            print("Lütfen tam sayı girişi yapınız")
+    if karar == 1:
+        kdv_miktari = toplam_tutar * (kdv/100)
+        eklenmis_tutar = toplam_tutar + kdv_miktari
+        print("\n--- SONUÇ ---")
+        print(f"KDV Eklenmiş fiyat: {eklenmis_tutar} TL")
+    else:
+        kdv_miktari = toplam_tutar * (kdv/100)
+        cikarilmis_tutar = toplam_tutar - kdv_miktari
+        print("\n--- SONUÇ ---")
+        print(f"KDV Çıkarılmış fiyat: {cikarilmis_tutar} TL")
+
+
 if kullanici_secimi == 1:
     basit_faiz_hesaplayici()
 if kullanici_secimi == 2:
     taksit_hesapla()
 if kullanici_secimi == 3:
-    None
+    kdv_hesapla()
 else:
     SystemExit
